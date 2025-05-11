af66620c2eaa8d48c817a413e611c10936bc876b
Kainoa Kanter <kainoa@t1c.dev>
2025-05-11 12:35:11 -0700

    disable landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.location.replace('https://inertia.social')
+})
+</script>
+
 <template>
   <main class="flex flex-col justify-center">
     <HomeTwitter />
